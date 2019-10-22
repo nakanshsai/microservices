0fc027dba7dcfa6eecf719d5a2337e1768dbae29
nakanshsai <55914116+nakanshsai@users.noreply.github.com>
2019-10-22 01:57:10 -0700

    Add files via upload

---
+++ Forex-service/target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into exchange_value
+(id,currency_from,currency_to,conversion_multiple,port)
+values(1001,'usd','inr',65,0);
+
+insert into exchange_value
+(id,currency_from,currency_to,conversion_multiple,port)
+values(1002,'eur','inr',75,0);
+
+insert into exchange_value
+(id,currency_from,currency_to,conversion_multiple,port)
+values(1003,'aud','inr',25,0);
